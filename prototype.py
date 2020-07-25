966d6b36f4fafb8996e6d639144700d097d69e42
BeastX889 <63514855+BeastX889@users.noreply.github.com>
2020-07-25 21:30:29 +0530

    Add files via upload

---
+++ prototype.py
@@ -0,0 +1,20 @@
+name=str(input("Enter your name:"))
+num=int(input("Enter number from 1 to 10:"))
+if name=="Ayush":
+    if num==1:
+        print("Fuckyou")
+    elif num==2:
+        print("Again")
+    elif num==3:
+        print("ok")
+    else:
+        print("bye")
+elif name =="Rangra":
+    if num==1:
+        print("kya")
+    elif num==2:
+        print("nhi")
+    else:
+        print("thik hay")
+else:
+    print("NOOOOOOO")
